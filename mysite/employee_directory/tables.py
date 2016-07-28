3e3c74925233916c075434553e9064ec0068c6d6
Maria Ines Aranguren <marangu1@nd.edu>
2016-07-28 16:25:43 -0500

    Sorting table and filtering employees on same view. Hide id column. Restrict sorting on selected columns.

--- mysite/employee_directory/tables.py
+++ mysite/employee_directory/tables.py
@@ -2,6 +2,9 @@ import django_tables2 as tables
 from employee_directory.models import Employee
 
 class EmployeeTable(tables.Table):
+    image = tables.Column(orderable=False)  #Restricts ordering for this column
+    phone_number = tables.Column(orderable=False)
+    email = tables.Column(orderable=False)
     class Meta:
         model = Employee
         # add class="paleblue" to <table> tag
