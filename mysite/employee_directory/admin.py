409f8a4f16326f7be9daa07f74d791f9b249d345
Maria Ines Aranguren <marangu1@nd.edu>
2016-08-12 16:29:52 -0500

    Cleaned up

--- mysite/employee_directory/admin.py
+++ mysite/employee_directory/admin.py
@@ -4,6 +4,7 @@
 from django.contrib import admin
 from django.contrib.admin import AdminSite
 from .models import Employee
+from django.forms import ModelForm
 
 admin.site.site_header = "Wizeline Employee Directory Administration"
 admin.site.site_title = "Wizeline Employee Directory"
@@ -14,35 +15,14 @@ class MyAdminSite(AdminSite):
 admin_site = MyAdminSite(name='admin')
 
 class EmployeeAdmin(admin.ModelAdmin):
-    # def linked_first_name(self, employee):
-    #     if employee.first_name:
-    #         # change_employee_directory_page = 'admin:%s_%s_change' % (employee._meta.app_label, employee._meta.module_name)
-    #         url = reverse(change_employee_directory_page, args=(employee.employee_directory.id,))
-    #         return format_html(u'<a href="{}">{}</a>', url, employee.employee_directory.first_name)
-    #     return ''
-    # linked_first_name.short_description = 'First name'
-
     list_display = (
         'id',
-        'image',
-        # 'admin_image',
-        # 'linked_first_name',
         'first_name',
         'last_name',
         'title',
         'team',
         'manager',
         'email',
-        # 'phone_number',
-        # 'start_date',
-        # 'address',
-        # 'personal_email',
-        # 'birth_date',
-        # 'blood_type',
-        # 'allergies',
-        # 'emergency_contact',
-        # 'created_at',
-        # 'updated_at'
     )
     list_filter = (
         'team',
@@ -50,7 +30,5 @@ class EmployeeAdmin(admin.ModelAdmin):
     )
     search_fields = ['first_name', 'last_name']
     ordering = ('first_name',) # Set default ordering to A-Z by first_name
-    # change_form_template = 'admin/change_form.html'
-
 
 admin.site.register(Employee, EmployeeAdmin)
